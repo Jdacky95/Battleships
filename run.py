d0d2c27d88bb6986b40224a798d4ace728ba13fa
Jdacky95 <jamesdack88@gmail.com>
2022-09-15 14:36:33 +0000

    First commit with placeholder text an input for user name and f string to incorporate this data into the game

--- run.py
+++ run.py
@@ -1,3 +1,27 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("Welcome to Battleships\n\n")
+
+print("rules\n\n")
+
+name = input("What would you like to be reffered to as for the purpose of this mission?\n\n")
+
+print("\n")
+
+print(f"welcome {name} , we hope you will guide us well in this war!")
+
+
+# class Board:
+
+#     for x in range(8):
+#         print(-)
+
+#     def __init__(self, row, column):
+#         self.row = row
+#         self.column = column
+
+    
+# class Player:
+#     def __init__(self, name, selection)
