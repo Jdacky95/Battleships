e4e86cc281396c8eb868e2b28595a06ff3ab04c5
Jdacky95 <jamesdack88@gmail.com>
2022-09-20 22:13:50 +0000

    Finish create ships function to populate game board and create function for recieving user guesses

--- run.py
+++ run.py
@@ -5,9 +5,10 @@ from random import randint
 
 # GUESSES_LEFT = 10
 
-# TOTAL_SHIPS = 5
+TOTAL_SHIPS = 8
 
-BOARD = [[" W "] * 8 for x in range(8)]  
+SHOWN_BOARD = [["W"] * 8 for x in range(8)]
+SECRET_BOARD = [["W"] * 8 for x in range(8)]
 
 
 def make_game_board(board):
@@ -15,61 +16,76 @@ def make_game_board(board):
     """
     function to create Game Board
     """
-
     print("   1   2   3   4   5   6   7   8")
     print("   -----------------------------")
 
-    # for A in range(0, 7):
-    #     board.append([" W "] * 8)
-    #     print(board)    
-
     row_num = 1
     for row in board:
-        print(f"{row_num}", "|".join(row))
+        print(f"{row_num}", " | ".join(row))
         row_num += 1
+        # board[2][2] = "S"    
     
 
-make_game_board(BOARD)
-    # for B in board:
-    #     print("".join(B))
+def create_ships(board):
+
+    """
+    function to create computer ships
+    """
+    for S in range(8):
+        # ship_row, ship_col = randint(0, 7), randint(0,7)
+        ship_row, ship_col = randint(0, 7), randint(0, 7)
+        board[ship_row][ship_col] = "S"
         
-    
-# def create_ships(board):
 
-#     """
-#     function to create computer ships
-#     """
-#     for ship in range(5):
-#         ship_row, ship_col = randint(0, 7), randint(0,7)
-#         board.append(ship,row, ship,col)
-    
-
-# def user_guesses(board):
-
-#     """
-#     Input function for user to place guesses:
-#     """
-#     user_guess_row = input("Enter your row coordinate: ")
-#     user_guess_column = input("Enter your column coordinate: ")
-#     if board [row] [column] == "W":
-#         board [row] [columm] = "X"
+# create_ships(SHOWN_BOARD)       
+# # create_ships(SECRET_BOARD)
+# make_game_board(SHOWN_BOARD)
 
 
-# def new_game(board):
-#     make_game_board(board)
-#     create_ships(board)
-#     user_guesses(board)
+def user_guesses(board):
+
+    """
+    Input function for user to place guesses:
+    """
+    user_guess_row = int(input("Enter your row coordinate: "))
+    user_guess_column = int(input("Enter your column coordinate: "))
+    if board[user_guess_row][user_guess_column] == "W":
+        board[user_guess_row][user_guess_column]= "M"
+    elif board[user_guess_row][user_guess_column] == "S":
+         board[user_guess_row][user_guess_column]= "H"
+
+         
+create_ships(SHOWN_BOARD) 
+make_game_board(SHOWN_BOARD)
+user_guesses(SHOWN_BOARD)
+create_ships(SHOWN_BOARD)       
+make_game_board(SHOWN_BOARD)
+
+
+
+# # def new_game(board):
+# #     make_game_board(board)
+# #     create_ships(board)
+# #     user_guesses(board)
     
 
 # print("Welcome to Battleships\n\n")
 
 # print("rules\n\n")
 
-# name = input("What would you like to be reffered to as for the purpose of this mission?\n\n")
+# name = input("What is your name Captain?\n\n")
 
 # print("\n")
 
 # print(f"Welcome {name} , we hope you will guide us well in this war!")
+
+# go = input("Press S to start game   ")
+
+# if go == "S":
+    # create_ships(BOARD)
+    # make_game_board(BOARD)
+# else:
+#     print("Damn Daniel, at it again with the white vans")
 
 
 # new_game(board)
